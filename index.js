46df2c836d197c6b368a2d8fcaa264bd87501d40
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-03-02 04:06:25 +0530

    Add ordering functionality, with form response as well as API response

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const mainRouter = require("./routes/main");
 const sidesRouter = require("./routes/sides");
+const orderRouter = require("./routes/order");
 
 const app = express();
 
@@ -13,19 +14,19 @@ const rootPath = app.get("project-root-path");
 
 app.use(express.static(path.join(rootPath, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(rootPath, "views", "index.html"));
-});
-
-
 app.use("/sides", sidesRouter); // kept above, to prevent premature handling by mainRouter
 app.use("/main", mainRouter);
 // IMPORTANT: this is a callback, don't call it.
 // pluggable middlewares need to be called,
 // since they return a configured middleware(based on arguments passed, if any)
+app.use(["/order", "/api/order"], orderRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(rootPath, "views", "index.html"));
+});
 
 app.use((req, res, next) => {
-  res.sendFile(path.join(rootPath, "views", "404.html"));
+  res.status(404).sendFile(path.join(rootPath, "views", "404.html"));
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
---
+++ routes/order.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("node:path");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.sendFile(
+    path.join(req.app.get("project-root-path"), "views", "place-order.html")
+  );
+});
+
+router.use(express.json());
+router.use(express.urlencoded());
+
+router.post("/", (req, res) => {
+  Object.keys(req.body).forEach((key) => {
+    req.body[key] = parseInt(req.body[key]) || 0;
+  });
+
+  if (req.is("urlencoded") || req.is("form-data")) {
+    res
+      .status(200)
+      .send("Order placed successfully" + JSON.stringify(req.body));
+  } else if (req.is("json")) {
+    res.status(200).json({
+      message: "Order placed",
+      orderDetails: req.body,
+    });
+
+    // copy and paste in browser console to test /api endpoint
+    const browserTestCode = `
+      fetch(
+        'http://localhost:3000/api/order',
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({burgers: 2})
+        })
+    `;
+  }
+});
+
+module.exports = router;

b0cd454943d3681bc5a642778c6109782e6b7877
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-03-02 01:44:58 +0530

    Add import lesson, middleware specification are callbacks

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ const rootPath = app.get("project-root-path");
 
 app.use(express.static(path.join(rootPath, "public")));
 
-app.use("/sides", sidesRouter);
+app.use("/sides", sidesRouter); // kept above, to prevent premature handling by mainRouter
 app.use("/", mainRouter);
+// IMPORTANT: this is a callback, don't call it.
+// pluggable middlewares need to be called,
+// since they return a configured middleware(based on arguments passed, if any)
 
 app.use((req, res, next) => {
   res.sendFile(path.join(rootPath, "views", "404.html"));
